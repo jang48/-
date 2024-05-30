0ea211b669982774643b2b6f037a51a52970f6e5
jang48 <jjangmi4843@gmail.com>
2024-05-30 23:55:55 +0900

    행정구역 표현완효

--- src/main/java/project/weather/weatherController.java
+++ src/main/java/project/weather/weatherController.java
@@ -29,26 +29,79 @@ public class weatherController {
     }
 
 //    @GetMapping("/weatherData")
-//    @ResponseBody
-//    public List<WeatherDTO> getWeatherData(@RequestParam("nx") double nx, @RequestParam("ny") double ny, Model model) throws IOException, JSONException {
-//        LatxLnY.LatXLngY info = this.latxLnY.convertGRID_GPS(0, nx, ny);   // daum 주소에 따른 적도,위도를 기상청 적도,위도로 변경
-//        List<WeatherDTO> weatherDTOList = this.apiExplorer.getWeatherInfo(info.x,info.y); // 기상청 api에 적도,위도 적용하여 초단기예보 가져오기
-//        return weatherDTOList;
+//    public String getWeatherData(@RequestParam("nx") String nx, @RequestParam("ny") String ny, @RequestParam("address") String address, RedirectAttributes redirectAttributes) {
+//        double nxDou = Double.parseDouble(nx);
+//        double nyDou = Double.parseDouble(ny);
+//        LatxLnY.LatXLngY info = this.latxLnY.convertGRID_GPS(0, nxDou, nyDou);   // daum 주소에 따른 적도,위도를 기상청 적도,위도로 변경
+//        redirectAttributes.addAttribute("x", info.x);
+//        redirectAttributes.addAttribute("y", info.y);
+//        redirectAttributes.addAttribute("nx", nx);
+//        redirectAttributes.addAttribute("ny", ny);
+//        redirectAttributes.addAttribute("address", address);
+//        return "redirect:/weatherData/test/";
 //    }
 
     @GetMapping("/weatherData")
-    public String getWeatherData(@RequestParam("nx") String nx, @RequestParam("ny") String ny, RedirectAttributes redirectAttributes) {
+    public String getWeatherData(@RequestParam("nx") String nx, @RequestParam("ny") String ny, @RequestParam("address") String address, Model model) throws JSONException, IOException {
         double nxDou = Double.parseDouble(nx);
         double nyDou = Double.parseDouble(ny);
         LatxLnY.LatXLngY info = this.latxLnY.convertGRID_GPS(0, nxDou, nyDou);   // daum 주소에 따른 적도,위도를 기상청 적도,위도로 변경
-        redirectAttributes.addAttribute("x", info.x);
-        redirectAttributes.addAttribute("y", info.y);
-        return "redirect:/weatherData/test/";
+        List<WeatherDTO> weatherDTOList = this.apiExplorer.getWeatherInfo(info.x, info.y);
+
+        List<List<WeatherDTO>> dtoList = new ArrayList<>();
+        int startIndex = 0;
+        int endIndex = 12;
+
+        for (int i = 0; i < weatherDTOList.size() / 12; i++) {
+            dtoList.add(weatherDTOList.subList(startIndex, Math.min(endIndex, weatherDTOList.size())));
+            startIndex += 12;
+            endIndex += 12;
+        }
+
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = today.plusDays(1);
+        LocalDate dayAfterTomorrow = today.plusDays(2);
+
+        WeatherDTO todayData = getFirstDataByDate(weatherDTOList, today);
+        WeatherDTO tomorrowData = getFirstDataByDate(weatherDTOList, tomorrow);
+        WeatherDTO dayAfterTomorrowData = getFirstDataByDate(weatherDTOList, dayAfterTomorrow);
+
+        WeatherDTO nowWeather = weatherDTOList.get(2);
+
+        model.addAttribute("today",todayData.getFcstDate());
+        model.addAttribute("today2",Objects.equals(todayData.getTMN(), null) ? '0' : todayData.getTMN());
+        model.addAttribute("today3",Objects.equals(todayData.getTMN(), null) ? '0' : todayData.getTMX());
+
+        model.addAttribute("tomorrow",tomorrowData.getFcstDate());
+        model.addAttribute("tomorrow2",tomorrowData.getTMN());
+        model.addAttribute("tomorrow3",tomorrowData.getTMX());
+
+        model.addAttribute("dayAfterTomorrow",dayAfterTomorrowData.getFcstDate());
+        model.addAttribute("dayAfterTomorrow2",Objects.equals(dayAfterTomorrowData.getTMN(), "") ? '0' : dayAfterTomorrowData.getTMN());
+        model.addAttribute("dayAfterTomorrow3",Objects.equals(dayAfterTomorrowData.getTMN(), "") ? '0' : dayAfterTomorrowData.getTMX());
+
+        model.addAttribute("dtoList", dtoList);
+        model.addAttribute("nowWeather", nowWeather);
+        model.addAttribute("latitude", nx);
+        model.addAttribute("longitude", ny);
+        model.addAttribute("address", address);
+
+        // 현재 시간의 정각시간부터 표현하기 위해 데이터 제공
+        LocalDateTime now = LocalDateTime.now();
+        now = now.withMinute(0);
+        String currentTime = now.format(DateTimeFormatter.ofPattern("HHmm"));
+        String currentDay = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        model.addAttribute("currentTime", currentTime);
+        model.addAttribute("currentDay", currentDay);
+        model.addAttribute("currentDay", currentDay);
+        model.addAttribute("currentDay", currentDay);
+
+        return "test";
     }
 
 
     @RequestMapping("/weatherData/test/")
-    public String handleWeatherData(@RequestParam("x") double x, @RequestParam("y") double y, Model model) throws JSONException, IOException {
+    public String handleWeatherData(@RequestParam("x") double x, @RequestParam("y") double y, @RequestParam("nx") String nx, @RequestParam("ny") String ny, @RequestParam("address") String address, Model model) throws JSONException, IOException {
         List<WeatherDTO> weatherDTOList = this.apiExplorer.getWeatherInfo(x, y); // 기상청 api에 적도,위도 적용하여 초단기예보 가져오기
 
         List<List<WeatherDTO>> dtoList = new ArrayList<>();
@@ -85,6 +138,9 @@ public class weatherController {
 
         model.addAttribute("dtoList", dtoList);
         model.addAttribute("nowWeather", nowWeather);
+        model.addAttribute("latitude", nx);
+        model.addAttribute("longitude", ny);
+        model.addAttribute("address", address);
 
         // 현재 시간의 정각시간부터 표현하기 위해 데이터 제공
         LocalDateTime now = LocalDateTime.now();
@@ -96,7 +152,7 @@ public class weatherController {
         model.addAttribute("currentDay", currentDay);
         model.addAttribute("currentDay", currentDay);
 
-        return "main";
+        return "test";
     }
 
     private WeatherDTO getFirstDataByDate(List<WeatherDTO> dtoList, LocalDate date) {
